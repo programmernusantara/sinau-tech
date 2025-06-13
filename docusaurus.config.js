9be94663221adc7548384b261b9e1ac68b5f898b
programmernusantara <pronus.programmernusantara@gmail.com>
2025-06-13 15:05:07 +0700

    bismilla

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,16 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://programmernusantara.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/sinau-tech/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'programmernusantara', // Usually your GitHub org/user name.
+  projectName: 'sinau-tech', // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
