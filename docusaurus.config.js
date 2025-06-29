e1d95fb9c88fcc240d404a652c24d15e5981d4ab
programmernusantara <pronus.programmernusantara@gmail.com>
2025-06-29 14:27:35 +0700

    First Commi

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,9 +93,8 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/programmernusantara/',
             label: 'GitHub',
             position: 'right',
           },
@@ -125,10 +124,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/programmernusantara',
